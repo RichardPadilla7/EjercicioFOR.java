d572a1bcb273d2df055f0d43b8733f67c3cffd70
Richard Padilla <150530717+RichardPadilla7@users.noreply.github.com>
2024-10-05 11:21:28 -0500

    richard p

--- src/EjercicioFor.java
+++ src/EjercicioFor.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+//Richard Padilla
 
 public class EjercicioFor {
+    public static void main(String[] args) {
+
+        int limite = 5;
+
+        for (int i = 0; i < limite; i++) {
+            System.out.println(i + 1);
+        }
+    }
 }
